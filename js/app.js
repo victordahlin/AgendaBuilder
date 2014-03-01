44a20366255530ec34022d6139bb55c5f8f6057b
Victor <victordahlin@gmail.com>
2014-03-01 17:53:04 +0100

    init

---
+++ js/app.js
@@ -0,0 +1,11 @@
+$(function() {
+	//The global variable so we can access it from other controller and views
+	//window.stage = "starter";
+	
+	//We instantiate our model
+	
+	// Views
+
+
+	// Controllers
+});
