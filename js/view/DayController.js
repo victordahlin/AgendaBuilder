82d6b78012f140b2e5de71ed1e0cf4d762500f19
Joakim Candefors <joakim.candefors@gmail.com>
2014-03-09 03:08:48 +0100

    Revert "merging"
    This reverts commit a30a9f3851084ce85280a7f28f87c07c158e15a9, reversing
    changes made to 492a7292d896f36eb4fd626858c2a6de80751811.

--- js/view/DayController.js
+++ js/view/DayController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var DayController = function(view, model) {	
 	
 	/*view.dayActivity.droppable({
@@ -8,22 +7,6 @@ var DayController = function(view, model) {
 			console.log(view.dayID);
 			//model.moveActivity(oldday, oldposition, newday, newposition)
 			//model.moveActivity(e.target, ui.draggable.id, view.dayID, ???);
-=======
-var DayController = function(view, model ) {	
-
-	// If user drop inside div and prevent select again
-	this.dayAct = function() {
-		//var array = model.getParkedActivities();
-		view.dayActivity.droppable({
-			drop: function( e, ui ) {
-				//model.removeParkedActivity(1);
-                // Får fixa tiderna på nått sätt
-				$("#addDayStartTimeBox").html("0" + model.days[0].getStart() + "0");
-                $("#addDayEndTime").html("End time: 0" + model.days[0].getEnd() + "0");
-                $("#addDayTotalLength").html("Total length: 0" + model.days[0].getEnd() + "0");
-
-                //console.log("Day Start: " + model.days[0].getStart());
->>>>>>> fc4bb41c3660f1549ae5847cf2dcd66f2b8e2550
 			}
 		}); */
 
