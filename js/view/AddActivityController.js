4645ad9d4f7cdcbc26ba4d3195f9a43a02742507
amandaglass <amandaglass@live.se>
2014-03-17 14:27:40 +0100

    added input control

--- js/view/AddActivityController.js
+++ js/view/AddActivityController.js
@@ -17,18 +17,24 @@ var AddActivityController = function(view, model) {
 			var typeid = $("#typeid").val();
 			var description = $("#description").val();
 
-			if(name!="" && length!="" && typeid!="" && description!=""){
-				var act = new Activity(name,length,typeid,description);
-				model.addParkedActivity(act);
-				
-				$("#popup").hide();
-				$("#activitiesContainer").show();
-				$("#addButtonContainer").show();
-				$("#addDayButton").show();
+
+   			if((parseFloat(length) == parseInt(length)) && !isNaN(length)){
+
+				if(name!="" && length!="" && typeid!=""){
+					var act = new Activity(name,length,typeid,description);
+					model.addParkedActivity(act);
+					
+					$("#popup").hide();
+					$("#activitiesContainer").show();
+					$("#addButtonContainer").show();
+					$("#addDayButton").show();
+				}
+				else{
+					alert("Fill all o' them boxes");
+				}
 			}
-			else{
-				alert("Fill all o' them boxes");
-			}
+			else { 
+				alert("Length must be a whole number")}
 		}
 	);
 	
